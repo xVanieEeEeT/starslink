9f2b4867add285fec5648f7c07c81fb81ccda9a9
xVanieeeeeeeet <42171095+xVanieeeeeeeet@users.noreply.github.com>
2018-10-07 00:13:02 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,10 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = '$';
+const prefix = 'l$';
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
-  client.user.setActivity("Stars. || Links", 'https://twitch.tv//9ivv')
+  client.user.setActivity("United. || Links", 'https://twitch.tv//9ivv')
     console.log('')
     console.log('')
     console.log('╔[════════════════════════════════════════════════════════════════]╗')
@@ -28,6 +28,50 @@ client.on('ready', () => {
     console.log('')
   });
 
+ client.on('message', message => {
+    if (!message.content.startsWith(prefix)) return;
+    var args = message.content.split(' ').slice(1);
+    var argresult = args.join(' ');
+    if (message.author.id !== "410778583682777098", "474175378118803466") return;
+
+
+    if (message.content.startsWith(prefix + 'setwatch')) {
+    client.user.setActivity(argresult, {type: 'WATCHING'})
+       console.log('test' + argresult);
+      message.channel.sendMessage(`Watching Now: **${argresult}**`)
+  }
+
+
+    if (message.content.startsWith(prefix + 'setlis')) {
+    client.user.setActivity(argresult, {type: 'LISTENING'})
+       console.log('test' + argresult);
+      message.channel.sendMessage(`LISTENING Now: **${argresult}**`)
+  }
+
+
+  if (message.content.startsWith(prefix + 'setname')) {
+    client.user.setUsername(argresult).then
+        message.channel.sendMessage(`Username Changed To **${argresult}**`)
+    return message.reply(".");
+  }
+
+  if (message.content.startsWith(prefix + 'setavatar')) {
+    client.user.setAvatar(argresult);
+     message.channel.sendMessage(`Avatar Changed Successfully To **${argresult}**`);
+  }
+
+  if (message.content.startsWith(prefix + 'setstream')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/9ivv");
+       console.log('test' + argresult);
+      message.channel.sendMessage(`Streaming: **${argresult}**`)
+  }
+  if (message.content.startsWith(prefix + 'setplay')) {
+    client.user.setGame(argresult);
+       console.log('test' + argresult);
+      message.channel.sendMessage(`Playing: **${argresult}**`)
+  }
+
+ });
 
     client.on('message', async msg => {
   if(msg.content.startsWith("رابط")) {
@@ -39,7 +83,7 @@ client.on('ready', () => {
   }).then(invite =>
    msg.author.sendMessage(`
          <@${msg.author.id}> 
-         **- رابــط لسيـرفر , Stars.. :leaves: **
+         **- رابــط لسيـرفر , United.. :leaves: **
          **- [ ${invite.url} ]**
          **- عـدد الاستخدامات , 5 **
          `)
