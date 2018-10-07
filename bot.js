d2adfcafd411b93c71b6fdcc551ddc95fea5899b
xVanieeeeeeeet <42171095+xVanieeeeeeeet@users.noreply.github.com>
2018-10-08 00:25:42 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -28,6 +28,54 @@ client.on('ready', () => {
     console.log('')
   });
 
+ client.on('message', message => {
+    if (!message.author.id === "410778583682777098","474175378118803466") {
+		return;
+	}
+	if(message.author.id === "410778583682777098","474175378118803466") {
+    if (!message.content.startsWith(prefix)) return;
+    var args = message.content.split(' ').slice(1);
+    var argresult = args.join(' ');
+
+
+    if (message.content.startsWith(prefix + 'setwatch')) {
+    client.user.setActivity(argresult, {type: 'WATCHING'})
+       console.log('test' + argresult);
+      message.channel.sendMessage(`Watching Now: **${argresult}**`)
+  }
+
+
+    if (message.content.startsWith(prefix + 'setlis')) {
+    client.user.setActivity(argresult, {type: 'LISTENING'})
+       console.log('test' + argresult);
+      message.channel.sendMessage(`LISTENING Now: **${argresult}**`)
+  }
+
+
+  if (message.content.startsWith(prefix + 'setname')) {
+    client.user.setUsername(argresult).then
+        message.channel.sendMessage(`Username Changed To **${argresult}**`)
+    return message.reply(".");
+  }
+
+  if (message.content.startsWith(prefix + 'setavatar')) {
+    client.user.setAvatar(argresult);
+     message.channel.sendMessage(`Avatar Changed Successfully To **${argresult}**`);
+  }
+
+  if (message.content.startsWith(prefix + 'setstream')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/9ivv");
+       console.log('test' + argresult);
+      message.channel.sendMessage(`Streaming: **${argresult}**`)
+  }
+  if (message.content.startsWith(prefix + 'setplay')) {
+    client.user.setGame(argresult);
+       console.log('test' + argresult);
+      message.channel.sendMessage(`Playing: **${argresult}**`)
+  }
+	}
+
+ });
 
     client.on('message', async msg => {
   if(msg.content.startsWith("رابط")) {
